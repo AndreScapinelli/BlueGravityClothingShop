e549004abc275bf6764d403018b37cb83f190615
zulks <andrescapinelli@hotmail.com>
2023-08-23 23:11:53 -0300

    Addons - Armour
    armor now has the full set (torso, shoulder, elbow and hands).
    
    new scriptableobject:
    - OutfitAddonArmor : OutfitAddonObject

---
+++ Clothing Project/Assets/Scripts/Scriptable Objects/OutfitAddonArmor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "outfit", menuName = "outfit/ new armour")]
+
+public class OutfitAddonArmor : OutfitAddonObject
+{
+    public Sprite leftShoulder;
+    public Sprite rightShoulder;
+
+    public Sprite leftElbow;
+    public Sprite rightElbow;
+
+    public Sprite leftHand;
+    public Sprite rightHand;
+}
